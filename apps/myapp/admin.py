fa80b374d360129dd1351907e4328ef010e52828
unknown <sultonabirqulov2022>
2022-07-15 09:51:39 +0500

    add contact

--- apps/myapp/admin.py
+++ apps/myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Contact
 # Register your models here.
+
+
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone']
+
+
+admin.site.register(Contact, ContactUsAdmin)
--- apps/myapp/models.py
+++ apps/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=125, null=True, blank=True)
+    phone = models.IntegerField(default=0)
+    email = models.EmailField(max_length=124, null=True, blank=True)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.name
